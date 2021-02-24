5cae9c1bdb75ae9b3f86e386f1e8168628a9e5d0
mani <mani@nibunar.com>
2021-02-24 17:46:58 +0530

    0.1.1

--- checkintent/mylibrary/src/main/java/com/nibunar/mylibrary/checkbox.java
+++ checkintent/mylibrary/src/main/java/com/nibunar/mylibrary/checkbox.java
@@ -6,11 +6,17 @@ import android.widget.Toast;
 
 public class checkbox  {
 
-   
+   static CheckBox checkbox;
 
     public static void s(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
-
+checkbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("toast","from library");
+                Toast.makeText(c,"checked",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
